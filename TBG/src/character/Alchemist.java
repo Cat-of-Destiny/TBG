300edc6c3b408780e25203798d2578e6c1d3171d
Cat-of-Destiny <weebley123@yahoo.co.uk>
2014-02-03 21:13:59 +0000

    Added an Alchemist and Soldier class, extends the Character class

---
+++ TBG/src/character/Alchemist.java
@@ -0,0 +1,11 @@
+package character;
+
+import java.util.Scanner;
+
+public class Alchemist extends Character {
+
+	public Alchemist() {
+		super(new Scanner(System.in));
+	}
+	
+}
---
+++ TBG/src/character/Soldier.java
@@ -0,0 +1,11 @@
+package character;
+
+import java.util.Scanner;
+
+public class Soldier extends Character {
+	
+	public Soldier() {
+		super(new Scanner(System.in));
+	}
+	
+}
