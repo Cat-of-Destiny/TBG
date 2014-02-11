a42d3df2d5a985a11d67c2e5366bd396ad291a1c
Cat-of-Destiny <weebley123@yahoo.co.uk>
2014-02-11 13:04:42 +0000

    Added the printSpells() to print out the spells of the mage

--- TBG/src/character/Mage.java
+++ TBG/src/character/Mage.java
@@ -3,6 +3,7 @@ package character;
 import java.util.Scanner;
 import java.util.Random;
 import library.Spell;
+import library.Methods;
 
 public class Mage extends Character {
 	
@@ -21,7 +22,7 @@ public class Mage extends Character {
 	//Create the initial spell
 	private void initialSpell(Random gen) {
 		
-		Spell initial = new Spell("Firebolt", gen);
+		Spell initial = new Spell("Firebolt");
 		
 		spells[0] = initial;
 		
@@ -47,16 +48,6 @@ public class Mage extends Character {
 		maxMana = mana;
 	}
 	
-	//Print out the spells
-	public void printSpells() {
-		
-		for (int i = 0; i < spells.length; i++) {
-			if (spells[i] != null) {
-				System.out.println(i + ") " + spells[i].getName());
-			}
-		}
-		
-	}
 	
 	//Print out the character's essential data
 	public void printData() {
@@ -66,4 +57,53 @@ public class Mage extends Character {
 		printSpells();
 	}
 	
+	//Method to print out mages spells
+	public void printSpells() {
+		
+		/*
+		 * Initialisation stage
+		 */
+		String[] names = new String[spells.length]; //The array with the names of the spells
+		int[] tabs = new int[names.length]; //An array of the amount of tabs needed for each name of the spells
+		int maxLength = 0; //Largest length of characters in the names
+		
+		for (int i = 0; i < spells.length; i++) { //Assign the names of the spells to the array of names
+			if (spells[i] != null) {
+				names[i] = spells[i].getName();
+			}
+		}
+		
+		for (int i = 0; i < names.length; i++) { //Get the amount of tabs in each name
+			if (names[i] != null) {
+				tabs[i] = (names[i].length() / 8) + 1;
+			}
+		}
+		
+		for (int i = 0; i < tabs.length; i++) { //Get the largest amount of tabs in the names
+			if (tabs[i] > maxLength) {
+				maxLength = tabs[i];
+			}
+		}
+		
+		for (int i = 0; i < tabs.length; i++) { //Get the actual amount of tabs to print
+			tabs[i] = (maxLength - tabs[i]) + 1;
+		}
+		
+		/*
+		 * Printing stage
+		 */
+		
+		System.out.print("Name");
+		Methods.printTabs(maxLength);
+		System.out.println("ID\tDamage\tMana Cost");
+		
+		
+		for (int i = 0; i < spells.length; i++) {
+			if (spells[i] != null) {
+				System.out.print(names[i]);
+				Methods.printTabs(tabs[i]);
+				System.out.println(i + "\t" + spells[i].getDmg() + "\t" + spells[i].getCost());
+			}
+		}
+	}
 }
