0dfd821bf4be239fd5371dc2411b9f241da65bc2
Cat-of-Destiny <weebley123@yahoo.co.uk>
2014-02-15 22:04:55 +0000

    Necromancer now has zombies

--- TBG/src/character/Necromancer.java
+++ TBG/src/character/Necromancer.java
@@ -6,8 +6,16 @@ public class Necromancer extends Character{
 	
 	Zombie[] zombies;
 	
+	
+	/**
+	 * Necromancer constructor
+	 */
 	public Necromancer() {
 		super();
+		
+		zombies = new Zombie[5]; //The zombie's the necromancer can use
+		zombies[0] = new Zombie("Rat", 3, 20, 1);
+		
 	}
 	
 }
